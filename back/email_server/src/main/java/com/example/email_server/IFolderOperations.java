14ce0f50da70dfd343b1ee2d022d8c0bbe899ed7
MariamOsman21 <76537563+MariamOsman21@users.noreply.github.com>
2021-12-27 15:40:14 +0200

    Update IFolderOperations.java

--- back/email_server/src/main/java/com/example/email_server/IFolderOperations.java
+++ back/email_server/src/main/java/com/example/email_server/IFolderOperations.java
@@ -1,7 +1,13 @@
 package com.example.email_server;
 
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public interface IFolderOperations {
-    void create();
-    void rename(String name);
-    void delete();
+    boolean create();
+    boolean rename(String name) throws IOException, ParseException;
+    void delete() throws IOException, ParseException;
+    public JSONArray loadMessages(int page);
 }
