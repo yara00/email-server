d7f18e79ee71b9e12cfaef0f0b70bd0d56e9a745
MariamOsman21 <76537563+MariamOsman21@users.noreply.github.com>
2021-12-28 03:05:38 +0200

    Delete back/email_server/src/main/java/com/example/email_server/filter directory

--- back/email_server/src/main/java/com/example/email_server/filter/ICriteria.java
+++
@@ -1,7 +0,0 @@
-package com.example.email_server.filter;
-
-import org.json.simple.JSONArray;
-
-public interface ICriteria {
-    public JSONArray meetCriteria(JSONArray mails, String criteriaValue);
-}
