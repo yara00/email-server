9b5e6caebb32ba0ee0995be62c7aee72ef20e17a
MariamOsman21 <76537563+MariamOsman21@users.noreply.github.com>
2021-12-28 02:37:15 +0200

    Delete IMailOperations.java

--- back/email_server/src/main/java/com/example/email_server/IMailOperations.java
+++
@@ -1,8 +0,0 @@
-package com.example.email_server;
-
-import org.json.simple.JSONObject;
-
-public interface IMailOperations {
-    public void add(String fileName, JSONObject message);
-    public void delete(String userName, String deleteFromFile , JSONObject message);
-}
